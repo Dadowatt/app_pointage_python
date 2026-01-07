568d3d8fbb0a4a12dfa15e9c3f589eac4d2f8368
Dadowatt <dadowatt92@gmail.com>
2026-01-07 15:40:53 +0100

    ajout de la fonction modifier_presence et afficherçapprenant

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def afficher_menu():
     print("2. Enregistrer la présence")
     print("3. Afficher les apprenants présents")
     print("4. Calculer le taux de présence")
-    print("5. Quitter")
+    print("5. Modifier le satut de présence")
+    print("6. Lister tous les apprenants")
+    print("7. Quitter")
 
 def ajouter_apprenant():
     while True:
@@ -25,7 +27,7 @@ def ajouter_apprenant():
             continue
 
     while True:    
-        nom = input("Nom : ")
+        nom = input("Nom : ").strip()
         if nom.isalpha():
             break
         print('Nom invalide')
@@ -51,6 +53,19 @@ def ajouter_apprenant():
     apprenants.append(apprenant)
     print("Apprenant ajouté avec succès!")
 
+def afficher_apprenant():
+    if not apprenants:
+        print("Aucun apprenant enregistré")
+        return
+    print("----- Liste de tous les apprenants ------")
+    for a in apprenants:
+        if a["presence"] is None:
+            statut = "Non pointé"
+        else:
+            statut = a["presence"].capitalize()
+        print(f" ID: {a['id']} | {a['prenom']} {a['nom']} | Statut: {statut}")
+
+
 def enregistrer_presence():
     if len(apprenants) == 0:
         print("Aucun apprenant.")
@@ -74,29 +89,58 @@ def afficher_presents():
     print("\n==== Apprenants présents ==== :")
     for a in apprenants:
         if a["presence"] == "présent":
-            print(f"- {a['prenom']} {a['nom']}")
+            print(f" ID: {a['id']} | {a['prenom']} {a['nom']}")
             found = True
     if not found:
         print("\nAucun apprenant présent")
 
+def modifier_presence():
+    if not apprenants:
+        print('Aucun apprenant présent')
+        return
+    try:
+        id_recherche = int(input("Entrer l'id de l'apprenant à modifier"))
+    except ValueError:
+        print("l'id doit être un nombre")
+        return
+    for a in apprenants:
+        if a["id"] == id_recherche:
+            if a["presence"] is None:
+                print("Cet apprenant n'est pas encore été pointé")
+                return
+            print(f"Statut actuel : {a['presence'].capitalize()}")
+
+            while True:
+                choix = input("Nouveau statut (p = présent / a = absent) : ").lower()
+                if choix == "p":
+                    a["presence"] = "présent"
+                    print("Statut modifié : Présent")
+                    return
+                elif choix == "a":
+                    a["presence"] = "absent"
+                    print("Statut modifié : Absent")
+                    return
+                else:
+                    print("Choix invalide")
+
+    print("Apprenant introuvable")
+
 def calculer_taux_presence():
+    if not apprenants:
+        print('Acun apprenant')
+        return
     presents = 0
     for a in apprenants:
         if a["presence"] == "présent":
             presents += 1
-
-    if len(apprenants) > 0:
-        taux = int((presents / len(apprenants)) * 100)
-        print(f"Taux de présence : {taux} %")
-    else:
-        print("Aucun apprenant.")
+    taux = int((presents / len(apprenants)) * 100)
+    print(f"Taux de présence : {taux} %")
 
 def main():
 
     while True:
         afficher_menu()
         choix = input("Choix : ")
-
         if choix == "1":
             ajouter_apprenant()
         elif choix == "2":
@@ -106,6 +150,10 @@ def main():
         elif choix == "4":
             calculer_taux_presence()
         elif choix == "5":
+            modifier_presence()
+        elif choix == "6":
+            afficher_apprenant()
+        elif choix == "7":
             print("Fin du programme")
             break
         else:
